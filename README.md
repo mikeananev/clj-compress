05af55ffbc11ce91e819b8960f404d2bacf1f2b9
Mike Ananev <mike1452@yandex.ru>
2019-03-03 02:27:33 +0300

    added create-archive

--- README.md
+++ README.md
@@ -1,12 +1,11 @@
 # clj-compress
 
-  A Clojure library designed to compress/decompress data.\
-  This is a thin wrapper for Apache commons-compress library.\
+  A Clojure library designed to compress/decompress data. This is a thin wrapper for Apache commons-compress library.\
   Supported algorithms: LZMA, GZIP, BZIP2, Snappy, Deflate, LZ4.
   
-  Best compression ratio (in order of ratio): BZIP2, LZMA, Deflate, GZIP.
+  Best compression ratio gives (in order of ratio): BZIP2, LZMA, Deflate, GZIP.
   
-  Snappy and LZ4 faster but has lower compression ratio.
+  Snappy and LZ4 faster but has lower compression ratio.\
   LZ4 on a big files sometimes very very slow.
 
 ## Usage
@@ -62,7 +61,22 @@ For Deps CLI add to deps.edn:  ```{:deps {middlesphere/clj-compress {:mvn/versio
     ```
     Don't forget to delete intermediate files.
 
-5. To create real archive:
+5. To create real archive from file or folder:
+
+    ```clojure
+    (create-archive "data/test-file.txt" "data/" "gz")
+    ;;=> "data/test-file.tar.gz"
+    
+    (create-archive "data/test-folder/" "data" "lzma")
+    ;;=> "data/test-folder.tar.lzma"
+    ```
+    
+6. To create real archive from folder:
+
+    ```clojure
+
+    ```
+ 
 
 ## Run Tests
 
--- src/middlesphere/clj_compress.clj
+++ src/middlesphere/clj_compress.clj
@@ -1,9 +1,19 @@
 (ns middlesphere.clj-compress
   (:require [clojure.java.io :refer [file output-stream input-stream] :as io])
   (:import (org.apache.commons.compress.compressors CompressorStreamFactory)
-           (org.apache.commons.compress.utils IOUtils)))
+           (org.apache.commons.compress.utils IOUtils)
+           (org.apache.commons.compress.archivers.tar TarArchiveOutputStream)
+           (java.io File)
+           (org.apache.commons.io FilenameUtils)))
 
 (def compressors ["lzma" "gz" "bzip2" "snappy-framed" "deflate" "lz4-framed"])
+
+(def archive-extensions {"lzma"          ".tar.lzma"
+                         "gz"            ".tar.gz"
+                         "bzip2"         ".tar.bz2"
+                         "snappy-framed" ".tar.snappy"
+                         "deflate"       ".tar.gz"
+                         "lz4-framed"    ".tar.lz4"})
 
 (defn compress-data
   "compress `src` using particular `compressor` and write compressed data into `dest`.
@@ -40,7 +50,7 @@
   (let [in    (input-stream src)
         f-out (output-stream dest)]
     (try
-      (let [in    (.createCompressorInputStream (CompressorStreamFactory.) decompressor in)
+      (let [in     (.createCompressorInputStream (CompressorStreamFactory.) decompressor in)
             length (IOUtils/copy in f-out)]
         (.flush f-out)
         (.close in)
@@ -50,3 +60,46 @@
         (.close in)
         (.close f-out)
         (throw e)))))
+
+
+(defn- new-archive-name
+  "return new archive name based on input parameters"
+  [input-name out-folder compressor]
+  (let [extension (get archive-extensions compressor)
+        fname     (FilenameUtils/getBaseName (FilenameUtils/normalizeNoEndSeparator input-name))]
+    (str (FilenameUtils/normalizeNoEndSeparator out-folder) "/" fname extension)))
+
+
+(defn- relativise-path
+  "create relative archive entry name"
+  [base path]
+  (let [f        (file base)
+        uri      (.toURI f)
+        relative (.relativize uri (-> path file .toURI))]
+    (.getPath relative)))
+
+
+(defn create-archive
+  "Create archive for given `input-name` (file or folder). An archive may be decompressed by external
+  tools (tar, unzip, bunzip2 etc...). New archive file will be placed to `out-folder`.
+  At first, input data is moved to tar archive, then tar archive is compressed by `compressor`.
+  An archive extension will be created during compression and depends on `compressor` type.
+  Returns created archive file name as `String`."
+  [^String input-name ^String out-folder ^String compressor]
+  (let [folder?   (.isDirectory (file input-name))
+        out-fname (new-archive-name input-name out-folder compressor)
+        fo        (output-stream out-fname)
+        cfo       (.createCompressorOutputStream (CompressorStreamFactory.) compressor fo)
+        a         (TarArchiveOutputStream. cfo)]
+    (doseq [f (if folder? (file-seq (file input-name)) [(file input-name)])]
+      (when (and (.isFile f) (not= out-fname (.getPath ^File f)))
+        (let [entry-name (relativise-path (FilenameUtils/getPath input-name) (-> f .getPath))
+              entry      (.createArchiveEntry a f entry-name)]
+          (.putArchiveEntry a entry)
+          (when (.isFile f)
+            (IOUtils/copy (input-stream f) a))
+          (.closeArchiveEntry a))))
+    (.finish a)
+    (.close a)
+    out-fname))
+
